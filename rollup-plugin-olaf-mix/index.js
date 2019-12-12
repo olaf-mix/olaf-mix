954883e1195f655c11173abdf1c192edb23a8aeb
cuppi <cuppi413@163.com>
2019-12-12 10:08:43 +0800

    优化 rollup 插件

--- rollup-plugin-olaf-mix/index.js
+++ rollup-plugin-olaf-mix/index.js
@@ -5,12 +5,16 @@ const {transformHandler} = require('../src/main');
 export default function olafMix (options = {}) {
     const filter = createFilter(options.include, options.exclude, {
     });
+    let forceInjected = false;
     return {
         name: 'olaf-mix',
+        load(id){
+            forceInjected = true;
+        },
         transform(code, id) {
             if (!filter(id)) return null;
             const root = j(code);
-            transformHandler(root);
+            transformHandler(root, {forceInjected});
             code = root.toSource({quote: 'single'});
             const result = { code: code };
             return result;
