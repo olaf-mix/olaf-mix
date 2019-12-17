43398be0db6de1d0ad2fb6e701c4310779f6f74f
cuppi <cuppi413@163.com>
2019-12-17 16:29:58 +0800

    增加 webpack 整合工具

---
+++ config/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const OlafMixLoader = require('../olaf-mix-loader/index');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    // mode: 'development',
+    mode: 'production',
+    entry: './input/index.js',
+    output: {
+        library: 'olaf_mix',
+        libraryTarget: 'umd',
+        filename: 'index._webpack.js',
+        path: path.resolve(__dirname, '..', 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                    'olaf-mix-loader',
+                ]
+            }
+        ]
+    },
+    resolveLoader: {
+        modules: ['node_modules', path.resolve(__dirname, '..')]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                minify: (file, sourceMap) => {
+                    const extractedComments = [];
+                    console.log(file);
+                    const { error, map, code, warnings } = require('uglify-js')
+                        .minify(file, {
+                            /* Your options for minification */
+                        });
+                    return { error, map, code, warnings, extractedComments };
+                },
+            }),
+        ],
+    }
+};
---
+++ olaf-mix-loader/index.js
@@ -0,0 +1,23 @@
+const j = require('jscodeshift');
+const path = require('path');
+const fs = require('fs');
+const {transformHandler} = require('../src/main');
+const config_path = path.resolve(process.cwd(), '.olaf.config.js');
+
+
+let config = null;
+if (fs.existsSync(config_path)){
+    config = require(config_path);
+} else {
+    config = require(path.resolve(__dirname, './.default.olaf.config.js'));
+}
+
+function OlafMixLoader(source) {
+    // Custom loader logic
+    const root = j(source);
+    transformHandler(root);
+    return root.toSource({quote: 'single'});
+}
+
+
+module.exports = OlafMixLoader;
