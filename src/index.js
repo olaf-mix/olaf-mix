17cfe06cc3bfaed18b9d4c126c2b0f2e0cd5d8f2
cuppi <cuppi413@163.com>
2019-12-18 12:00:06 +0800

    优化代码

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const j = require('jscodeshift');
 const {transformHandler} = require('./main')
-import * as log from 'loglevel';
+const log = require('loglevel');
 log.setLevel(process.env.RUN_MODE === 'debug' ? 'debug' : 'warn')
 
 module.exports = function(fileInfo, api, options){
-    console.log('transforming', fileInfo.path);
     const root = api.jscodeshift(fileInfo.source);
     transformHandler(root);
     return root.toSource({quote: 'single'});
